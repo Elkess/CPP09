b443176535021833c1efc7b82a10e51f61dc1b21
Elkess <mohamedamineel123@gmail.com>
2026-09-14 22:12:56 +0100

    version

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -46,13 +46,34 @@ void	fill_db(std::map<std::string, double> &db)
 	// }
 }
 
+bool	ft_isdigit(std::string str)
+{
+	size_t i=0;
+	while (str[i])
+	{
+		if (!std::isdigit(str[i]))
+			return false;
+		i++;
+	}
+	return true;
+}
+
+bool	is_valid_date(std::string date)
+{
+	if (date.size() != 10 || date[4] != '-' || date[7] != '-')
+		return false;
+	int year = std::atoi(date.substr(0, 4).c_str());
+	int month = std::atoi(date.substr(5, 7).c_str());
+	int day = std::atoi(date.substr(8, 10).c_str());
+	if (year < 2009 && month < 1 && day < 12)
+		return false;
+}
+
 void	bitcoinexchange(std::ifstream &file)
 {
 	std::string line;
 	while (std::getline(file, line))
 	{
-		if (line.size() != 10)
-			err_printer(" bad input => " + line);
 		size_t pos = line.find('|');
 		if (pos == std::string::npos || 
 			line[pos +1] != ' ' && line[pos -1])
@@ -62,10 +83,19 @@ void	bitcoinexchange(std::ifstream &file)
 		}
 		std::string date = line.substr(0, pos -1);
 		std::string val_str = line.substr(pos +1, line.size());
-		// khassni n parssi date o val
+		if (!ft_isdigit(val_str))
+			err_printer("Not A Number.");
+		if (is_valid_date(date))
+			err_printer("bad input => " + date);
+		
+		std::stringstream ss(val_str);
+		float val;
+		if (!(ss >> val) || val > 1000)
+			err_printer("too large a number.");
+		else if (val < 0)
+			err_printer("not a positive number.");
 	}
 }
-
 
 int	main(int ac, char **av)
 {
@@ -80,4 +110,4 @@ int	main(int ac, char **av)
 	}
 	else
 		return err_printer("could not open file."), 1;
-} 
+}
